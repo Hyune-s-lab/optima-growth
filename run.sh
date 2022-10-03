189e49877b7fb1759d99e9db423a2ec724f1f17c
hyune-c <choi8608@gmail.com>
2022-10-03 20:07:44 +0900

    :wrench: set docker-compose

--- run.sh
+++ run.sh
@@ -3,6 +3,5 @@ cd licensing-service
 ./gradlew bootjar
 cd ..
 
-docker build . --tag license-service
-
-docker run -it -p 8080:8080 license-service:latest
+docker build . --tag licensing-service
+docker-compose up
