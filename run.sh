2af71f58d0dfd8b4f3343f8202823b6102ea3aa2
hyune-c <choi8608@gmail.com>
2022-10-05 20:59:09 +0900

    :wrench: update docker setting about add eureka-server

--- run.sh
+++ run.sh
@@ -1,7 +1,13 @@
+# eureka-server
+cd eureka-server
+./gradlew bootjar
+cd ..
+docker build -f docker/eureka-server/Dockerfile -t eureka-server .
+
 # licensing-service
 cd licensing-service
 ./gradlew bootjar
 cd ..
+docker build -f docker/licensing-service/Dockerfile -t licensing-service .
 
-docker build . --tag licensing-service
-docker-compose up
+nohup docker-compose up 1>/dev/null 2>&1 &
